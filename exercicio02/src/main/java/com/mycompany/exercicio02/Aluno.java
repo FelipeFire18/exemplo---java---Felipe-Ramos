ac8ec2eeeb1f0bc29d5c997608ca00161daa1a18
FelipeFire18 <felipefire18@gmail.com>
2024-10-04 20:39:28 -0300

    add da classe aluno e adição de um metodo construtor

---
+++ exercicio02/src/main/java/com/mycompany/exercicio02/Aluno.java
@@ -0,0 +1,12 @@
+package com.mycompany.exercicio02;
+
+public class Aluno {
+    private int codigo;
+    private String nome;
+    private String endereço;
+    
+    Aluno(int c, String n){
+        this.codigo = c;
+        this.nome = n;
+    }
+}
