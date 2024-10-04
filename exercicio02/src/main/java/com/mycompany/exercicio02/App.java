dac676fce96a0222eee77797a9e8748f3bbf2b2a
FelipeFire18 <felipefire18@gmail.com>
2024-10-04 20:56:09 -0300

    Update App.java
    Instanciando as classes:
    Aluno.java
    Curso.java
    Turma.java
    Matricula.java

--- exercicio02/src/main/java/com/mycompany/exercicio02/App.java
+++ exercicio02/src/main/java/com/mycompany/exercicio02/App.java
@@ -1,5 +1,20 @@
 package com.mycompany.exercicio02;
 
 public class App {
-    
+    public static void main(String[] args) {
+        //instanciar todas as classes criadas
+        Aluno a = new Aluno(100, "fulano");
+        Aluno a2 = new Aluno(200, "Ciclano");
+        
+        Curso c1 = new Curso("Sistemas", 8);
+        Curso c2 = new Curso("Agronomia", 10);
+        
+        Turma t1 = new Turma("2° Período", c1);
+        Turma t2 = new Turma("2° Período", c2);
+        
+        Matricula m1 = new Matricula(20241, a, t1);
+        Matricula m2 = new Matricula(20242, a2, t2);
+        
+        //imprimir tudo que conseguir
+    }
 }
