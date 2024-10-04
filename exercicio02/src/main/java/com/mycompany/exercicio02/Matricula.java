8ddc4b7eb3fa1fbbfa5529275ac3a370c88bb75f
FelipeFire18 <felipefire18@gmail.com>
2024-10-04 20:49:10 -0300

    Add Matricula.java

---
+++ exercicio02/src/main/java/com/mycompany/exercicio02/Matricula.java
@@ -0,0 +1,13 @@
+package com.mycompany.exercicio02;
+
+public class Matricula {
+    private int numero;
+    private Aluno aluno;
+    private Turma turma;
+    
+    Matricula(int numero, Aluno aluno, Turma turma){
+        this.numero = numero;
+        this.aluno = aluno;
+        this.turma = turma;
+    }
+}
