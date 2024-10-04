367aac2c2eb99bfde1c0fa7ffed8d50ddbd63f3c
FelipeFire18 <felipefire18@gmail.com>
2024-10-04 20:42:38 -0300

    add Curso.java

---
+++ exercicio02/src/main/java/com/mycompany/exercicio02/Curso.java
@@ -0,0 +1,11 @@
+package com.mycompany.exercicio02;
+
+public class Curso {
+    private String nome;
+    private int duracao;
+    
+    Curso( String n, int d){
+        this.nome = n;
+        this.duracao = d;
+    }
+}
