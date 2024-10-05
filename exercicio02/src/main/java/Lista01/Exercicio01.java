25cdf832ce2128df30a9bae8b2a6588605d17fc3
FelipeFire18 <felipefire18@gmail.com>
2024-10-04 22:23:34 -0300

    Add scan para a entrada do nome

--- exercicio02/src/main/java/Lista01/Exercicio01.java
+++ exercicio02/src/main/java/Lista01/Exercicio01.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class Exercicio01 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         
+        System.out.println("Porfavor digite seu nome:");
+        String nome = scan.nextline();
     }
 }
