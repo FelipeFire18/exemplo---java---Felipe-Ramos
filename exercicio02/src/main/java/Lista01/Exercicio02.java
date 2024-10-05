29b7e5c870198d93e07b9e8b58792145f1b8dcf9
FelipeFire18 <felipefire18@gmail.com>
2024-10-04 22:32:27 -0300

    add scan

--- exercicio02/src/main/java/Lista01/Exercicio02.java
+++ exercicio02/src/main/java/Lista01/Exercicio02.java
@@ -5,5 +5,8 @@ import java.util.Scanner;
 public class Exercicio02 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Por favor nos informe seu nome:");
+        String nome = scan.nextLine();
     }
 }
