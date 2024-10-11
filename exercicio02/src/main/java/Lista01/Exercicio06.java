8498719dcfde6288bcde8ee80448625f28aa9cbc
FelipeFire18 <felipefire18@gmail.com>
2024-10-11 20:08:36 -0300

    Adição da classe scan

--- exercicio02/src/main/java/Lista01/Exercicio06.java
+++ exercicio02/src/main/java/Lista01/Exercicio06.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class Exercicio06 {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         
+        System.out.println("Por favor digite seu nome: ");
+        String nome = s.nextLine();
     }
 }
